78e0199d7f106d5602f63fb267231bc08b65fb55
DAN HEFLIN <Heftone@HEFTONE-MACBOOK.local>
2019-05-23 09:36:40 -0700

    Updates

---
+++ precache-manifest.c67b6b8149a22ba643805b501da03458.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5ee25fe96b469da4ba4ffa04ee29a7d",
+    "url": "/react-adventure/static/media/middleRightHome_USEALT.f5ee25fe.JPG"
+  },
+  {
+    "revision": "4a84080cd32df32445cc0fa2b72c6bb9",
+    "url": "/react-adventure/static/media/leftAbout_USE.4a84080c.jpg"
+  },
+  {
+    "revision": "61cf8c1a0e2b52e31343",
+    "url": "/react-adventure/static/js/runtime~main.f14d2d3a.js"
+  },
+  {
+    "revision": "076fe36a1e43776b13d1",
+    "url": "/react-adventure/static/js/main.4a79470a.chunk.js"
+  },
+  {
+    "revision": "1f7ec79111a6631284bb",
+    "url": "/react-adventure/static/js/2.74622314.chunk.js"
+  },
+  {
+    "revision": "076fe36a1e43776b13d1",
+    "url": "/react-adventure/static/css/main.362515ed.chunk.css"
+  },
+  {
+    "revision": "1f7ec79111a6631284bb",
+    "url": "/react-adventure/static/css/2.e7f6f7da.chunk.css"
+  },
+  {
+    "revision": "97a2670d22505514a78a491fd8af08ca",
+    "url": "/react-adventure/index.html"
+  }
+];
